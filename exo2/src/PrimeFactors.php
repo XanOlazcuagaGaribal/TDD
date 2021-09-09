6693ac39b2c6a33b4153cac5af73657bed083f10
Xan.olazcuagagaribal <xan.olazcuagagaribal@gmail.com>
2021-09-09 13:58:24 +0200

    All test ok, function refactored

--- exo2/src/PrimeFactors.php
+++ exo2/src/PrimeFactors.php
@@ -11,18 +11,26 @@ class PrimeFactors{
     Un nombre premier est un nombre entier supérieur ou égal à deux qui a
     exactement deux diviseurs : 1 et lui-même.*/
 
-    public static function generate(int $number){
+    public static function generate(int $number): array{
         $array = [];
 
-        if($number > 1){
-            if($number % 2 === 0){
+        /*if($number > 1){
+            while($number % 2 === 0){
                 $array[] = 2;
                 $number /= 2;
             }
             if($number > 1){
                 $array[] = $number;
             }
+        }*/
+
+        for($potentialDivider = 2; $potentialDivider <= $number; ++$potentialDivider){
+            while($number % $potentialDivider === 0){
+                $array[] = $potentialDivider;
+                $number /= $potentialDivider;
+            }
         }
+
         return $array;
     }
 }
--- exo2/tests/PrimeFactorsTest.php
+++ exo2/tests/PrimeFactorsTest.php
@@ -23,4 +23,27 @@ class PrimeFactorsTest extends TestCase
     {
         $this->assertEquals([2, 2], PrimeFactors::generate(4), "4 a 2 facteurs premiers");
     }
+
+    public function testFive()
+    {
+        $this->assertEquals([5], PrimeFactors::generate(5), "5 a un seul facteur premier");
+    }
+
+    public function testSeven()
+    {
+        $this->assertEquals([7], PrimeFactors::generate(7), "5 a un seul facteur premier");
+    }
+    public function testHeight()
+    {
+        $this->assertEquals([2, 2, 2], PrimeFactors::generate(8), "8 a 3 facteurs premiers");
+    }
+    public function testNine()
+    {
+        $this->assertEquals([3, 3], PrimeFactors::generate(9), "8 a 3 facteurs premiers");
+    }
+    public function test125()
+    {
+        $this->assertEquals([5, 5, 5], PrimeFactors::generate(125), "8 a 3 facteurs premiers");
+    }
+
 }
